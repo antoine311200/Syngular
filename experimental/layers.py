ea5b7196a64f0b82cb36fad630dd05df32c3ceb6
Antoine Debouchage <antoine311200@gmail.com>
2021-11-10 21:48:16 +0100

    Improving the layer approach thus having implemented a one real output in place of a tensor output... need to convert the process and do more research as it has not been implented yet in the litterature

--- experimental/layers.py
+++ experimental/layers.py
@@ -4,6 +4,8 @@ from re import match
 
 import numpy as np
 from numpy.core.einsumfunc import einsum
+from numpy.lib.function_base import _CORE_DIMENSION_LIST
+from numpy.matrixlib.defmatrix import matrix
 
 
 def unfold_shape(shape):
@@ -66,6 +68,10 @@ class Dense(TensorTrainLayer):
         self.cores = []
         self.bias = []
 
+        self.__bond_core = None
+        self.__bond_core_index = None
+        self.__bond_core_update_step = False
+
     def __get_core_shape(self, index):
         if index == 0 or index == self.cores_number-1:
             return (self.input_shape[index], self.output_shape[index], self.bond_dim,)
@@ -95,13 +101,143 @@ class Dense(TensorTrainLayer):
         else:
             raise Exception('the type of core to add does not match the current cores structure')
 
+    def __contract_bond_core(self, index):
+        if not self.__bond_core_update_step:
+            raise Exception("an updating step of the cores has not been started yet")
+        
+        if index >= self.cores_number or index < 0:
+            raise Exception(f"the index provided for contraction is out of bond ({index})")
+
+        __copy_cores = self.cores.copy()
+        current_core = __copy_cores[index]
+        next_core = __copy_cores.pop(index+1)
+
+        print(index, self.cores_number-2)
+        if index == 0:
+            __copy_cores[index] = np.einsum(current_core, [Ellipsis, 0], next_core, [Ellipsis, 0, 1])
+        elif index != self.cores_number-2:
+            __copy_cores[index] = np.einsum(current_core, [Ellipsis, 0, 1], next_core, [Ellipsis, 1, 2])
+        else:
+            __copy_cores[index] = np.einsum(current_core, [Ellipsis, 0, 1], next_core, [Ellipsis, 1])
+
+        self.__bond_core_index = index
+        self.__bond_core = __copy_cores[self.__bond_core_index]
+
+    def __project_wings(self, input, contracted_index):
+        if not self.__bond_core_update_step:
+            raise Exception("an updating step of the cores has not been started yet")
+        
+        input = np.array(input)
+        unfold_input = unfold_shape(input.shape)
+
+        if self.unfold_input_shape != unfold_input:
+            exception = f"input of shape {input.shape} cannot be reshaped into {self.input_shape} [{unfold_input} != {self.unfold_input_shape}]"
+            raise Exception(exception)
+
+        input_tensor = np.reshape(input, newshape=self.input_shape)
+        input_index = np.arange(self.cores_number)
+
+        einsum_structure = []
+        einsum_structure.append(input_tensor)
+        einsum_structure.append(input_index)
+
+        for idx in range(self.cores_number):
+            ipt_index = idx
+            opt_index = self.cores_number+idx
+
+            if idx == contracted_index or idx == contracted_index+1:
+                einsum_structure.append(self.cores[idx])
+                if idx == 0:
+                    bnd_index = 2*self.cores_number
+                    einsum_structure.append([ipt_index, opt_index, bnd_index])
+                elif idx == self.cores_number-1:
+                    bnd_index = 3*self.cores_number-2
+                    einsum_structure.append([ipt_index, opt_index, bnd_index])
+                else:
+                    bnd_index_1 = 2*self.cores_number+idx-1
+                    bnd_index_2 = 2*self.cores_number+idx
+                    einsum_structure.append([ipt_index, opt_index, bnd_index_1, bnd_index_2])
+
+        projected_tensor = np.einsum(*einsum_structure)
+
+        return projected_tensor
+
+    def __reset_bond_core(self):
+        self.__bond_core = None
+        self.__bond_core_index = None
+        self.__bond_core_update_step = False
+
+    # Possible error in the tuples of axes for reshaping the tensor into a matrix for SVD
+    def __SVD_bond_core(self, chi=None):
+        inp, out, left, right = self.__bond_core.shape
+        newdim = [inp * left, out * right]
+
+        print(self.__bond_core.shape) 
+        
+        # (in, out, left, right) -> ((in, left), (out, right))
+        
+        matrix_core_transposed = np.transpose(self.__bond_core, (0,2,1,3))
+        print(matrix_core_transposed.shape, newdim)
+        matrix_core = np.reshape(matrix_core_transposed, newdim, order='F')
+        u, s, v = np.linalg.svd(matrix_core, full_matrices=False)
+
+        u_trimmed = u[:,:chi]
+        s_trimmed = s[:chi]
+        v_trimmed = v[:chi,:]
+
+        print("SVD shapes")
+        print(u.shape)
+        print(s.shape)
+        print(v.shape)
+        print("SVD trimmed shapes")
+        print(u_trimmed.shape)
+        print(s_trimmed.shape)
+        print(v_trimmed.shape)
+
+        # current shape of u : ((in, left), chi) [ (a_j, (a_j-1, s_j)) = (a_j2, (a_j1, s_j1)) ]
+        # need to be converted to A : (s_j1, a_j1, a_j2)
+        # core_at_index_transposed = np.transpose(u_trimmed, (1,0))
+        core_at_index = np.reshape(u_trimmed, newshape=(left, inp, chi), order='F')
+        core_at_index = np.transpose(core_at_index, (1,0,2))
+
+        # current shape of s*v : (chi, (out, right))
+        # need to be converted to A : (out, right, chi)
+        core_next_index_transposed = np.transpose(s_trimmed * v_trimmed.T, (1,0))
+        core_next_index = np.reshape(core_next_index_transposed, newshape=(out, right, chi), order='F')
+
+        print("New cores shapes")
+        print(core_at_index.shape)
+        print(core_next_index.shape)
+
+    def __update_bond_core(self, index, grad_loss):
+        self.__bond_core_update_step = True
+        self.__contract_bond_core(index)
+
+        # For testing purposes for now
+        print(f"Bon dimension at index {index} is {self.__bond_core.shape[2]}")
+        self.__SVD_bond_core(chi=self.__bond_core.shape[2])
+
+        return self.__bond_core - self.learning_rate * grad_loss
+
+    def __crumble(self, projected_wings):
+        rank = len(projected_wings.shape)
+
+        if rank != 4 and (self.__bond_core_index != 0 or self.__bond_core_index != self.cores_number-2):
+            raise Exception('error while computing the projected input through the wings of the tensor train for a middle bond core')
+        elif rank != 3 and (self.__bond_core_index == 0 or self.__bond_core_index == self.cores_number-2):
+            raise Exception('error while computing the projected input through the wings of the tensor train for an extreme bond core')
+
+        if rank == 4:
+            return np.einsum(projected_wings, np.arange(4), self.__bond_core, np.arange(4))
+        elif rank == 3:
+            return np.einsum(projected_wings, np.arange(3), self.__bond_core, np.arange(3))
+        else:
+            raise Exception(f"an error occured with a mismatch of the rank of the projected input through the wings of the tensor train (rank = {rank}")
 
     def build(self):
         self.cores.append(self.__add_core(name='core_1', type='extreme'))
-
         for i in range(1, self.cores_number-1):
             self.cores.append(self.__add_core(name = "core_"+str(i), type='middle'))
-
         self.cores.append(self.__add_core(name='core_'+str(self.cores_number), type='extreme'))
 
         self.bias = np.zeros(shape=self.output_shape)
@@ -159,9 +295,11 @@ class Dense(TensorTrainLayer):
         result = contraction+self.bias
         print(result)
 
+        return result
 
-    def backward(self):
-        pass
+
+    def backward(self, input, grad_output):
+        self.__update_bond_core(1, 0)
 
     def train(self):
         pass
@@ -169,7 +307,7 @@ class Dense(TensorTrainLayer):
 
 if __name__ == "__main__":
 
-    layer = Dense((2,2), (3,3), bond_dim=2)
+    layer = Dense((2,2,2,2), (3,3,3,3), bond_dim=4)
     layer.build()
 
     print("Cores")
@@ -177,4 +315,17 @@ if __name__ == "__main__":
     print("Bias")
     print(layer.bias)
 
-    layer.forward([[1,4],[2,5]])
+    tensor = np.arange((2**4)).reshape(((2,2,2,2)))
+    layer.forward(tensor)
+
+    print('--------- Backpropagation ----------')
+
+    layer.backward(tensor, None)
+
+
+# HUGE ERROR : I am currently working with MPDO but I implemented a 
+# classification backpropagation (thus implementing a MPS)
+
+# I have to that into account the MPO or switch to a more drastic approach consisting
+# in creating n times a weight tensor to perform to each the classification approach and backpass
+# where n is the number of output (I do not think it is interesting at all)
--- tensor/tensor.py
+++ tensor/tensor.py
@@ -1,23 +1,31 @@
 from __future__ import annotations
 
 from typing import List, Union
-
-from ..utils.benchmark import Timer
+from numpy.core.fromnumeric import reshape
 
 import tensorflow as tf
 import numpy as np
 
-class Tensor:
+class TensorTrain:
 
     def __init__(self, tensor: Union[tf.Tensor, list]) -> None:
         self.tensor = tensor
 
-        self.shape = self.tensor.get_shape().as_list()
+        self.cores = []
+
+        self.shape = self.tensor.shape
     
-    def __add__(self, tensor: tf.Tensor) -> Tensor:
-        return Tensor(self.tensor + tensor)
+    def __add__(self, tensor: tf.Tensor) -> TensorTrain:
+        return TensorTrain(self.tensor + tensor)
     
-    @Timer.wrapper
+    def factorize(self, index_shape, index_names):
+        self.index_names = index_names
+        self.index_shape = index_shape
+
+        def mps():
+            pass
+
+
     def __getitem__(self, index):
         print(index)
         value = self.tensor
@@ -29,27 +37,81 @@ class Tensor:
         return value
 
 
-    def decompose(self, cores, bond_dim):
-        pass
+    def decompose_as_mpo(self, input_shape, output_shape, bond_shape):
+        self.cores = []
+
+        __current_tt = np.reshape(self.tensor.copy(), (*input_shape, *output_shape), order='F')
+
+        ipt_rank = len(input_shape)
+        opt_rank = len(output_shape)
+        bnd_rank = len(bond_shape)
+        rank = ipt_rank+opt_rank+bnd_rank
+
+        rest_ipt_dim = input_shape
+        rest_opt_dim = output_shape
+        rest_bnd_dim = bond_shape
+
+        cores_number = ipt_rank
+
+        for idx in range(cores_number-1):
+            bnd_dim = bond_shape[idx]
+            ipt_dim = input_shape[idx]
+            opt_dim = output_shape[idx]
+
+            rest_ipt_dim = rest_ipt_dim[1:]
+            rest_opt_dim = rest_opt_dim[1:]
+            rest_bnd_dim = rest_bnd_dim[1:]
+
+            print(__current_tt.shape)
+
+            reshape_tt = np.transpose(__current_tt, (0,2,1,3))
+            reshape_tt = np.reshape(reshape_tt, newshape=(ipt_dim*opt_dim, -1), order='F')
+            U, S, V = np.linalg.svd(reshape_tt, full_matrices=False)
+            
+            print(U.shape, S.shape, V.shape)
+
+            print((bnd_dim, *rest_ipt_dim, *rest_opt_dim))
+
+            S_round = S[:bnd_dim*2]
+            S_round = np.reshape(S_round, newshape=(bnd_dim, bnd_dim))
+            U_round = np.reshape(U, (ipt_dim, opt_dim, -1), order='F')[:,:,:bnd_dim]
+            V_round = np.reshape(V, (*rest_ipt_dim, *rest_opt_dim, -1), order='F')[:,:,:bnd_dim]
+            # V_round = np.transpose(V_round, (2,0,1))
+            
+            print(U_round.shape)
+            print(S_round.shape)
+            print(V_round.shape)
+
+            __current_tt = np.einsum(S_round, [0,1], V_round, [Ellipsis,1], [Ellipsis,0])
+            core = U_round
+
+            print(__current_tt.shape)
+
+            self.cores.append(core)
+        self.cores.append(__current_tt)
+
+        print(self.cores)
+
+        print("Core shape")
+        for c in self.cores:
+            print(c.shape)
+
+    def retrieve(self, index_list):
+        np.einsum()
 
     @staticmethod
     def empty(shape):
-        return Tensor(tf.Variable(np.empty(shape), dtype=np.float32))
+        return TensorTrain(tf.Variable(np.empty(shape), dtype=np.float32))
 
     ''' Simple implemtation for two tensors for now '''
     @staticmethod
-    def contract(a: Tensor, b: Tensor, index: List[List[float]]):
-        c = Tensor.empty((2,2))
-        print('A', a)
-        print(a.shape, b.shape)
-
-        # case index = [[x,y]]
-
-        shape = a.shape[:index[0][0]-1]+a.shape[index[0][0]:] + b.shape[:index[0][1]-1]+b.shape[index[0][1]:]
-        print(shape)
-
-        c = Tensor.empty(shape)
-        print(c.tensor)
-
+    def contract(a: TensorTrain, b: TensorTrain, index: List[List[float]]):
+        pass
         
 
+if __name__ == "__main__":
+    tensor = np.random.normal(size=(1024,1024))
+    # print(tensor)
+    A = TensorTrain(tensor)
+
+    A.decompose_as_mpo((32,32), (32,32), (2,))
